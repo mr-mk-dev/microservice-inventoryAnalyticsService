c5314c81112965dc437b805b6bee73774b9fe0ce
Manish <itsmenishan.12@gmail.com>
2025-11-14 09:21:25 +0530

    Initial Commit

---
+++ src/main/java/me/manishcodes/inventoryanalyticsservice/InventoryAnalyticsServiceApplication.java
@@ -0,0 +1,13 @@
+package me.manishcodes.inventoryanalyticsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryAnalyticsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryAnalyticsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/manishcodes/inventoryanalyticsservice/InventoryAnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.manishcodes.inventoryanalyticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAnalyticsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
